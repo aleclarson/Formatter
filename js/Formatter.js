cceb7c4032e33178103abb88efda3daf62663b98
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-08-27 04:03:59 -0700

    08/27/2016 4:03 AM

--- js/Formatter.js
+++ js/Formatter.js
@@ -1,10 +1,6 @@
-var Formatting, Tracer, Type, Validator, assertType, formatObject, formatValue, isType, steal, type;
+var Formatting, Type, assertType, formatObject, formatValue, isType, steal, type;
 
 assertType = require("assertType");
-
-Validator = require("Validator");
-
-Tracer = require("tracer");
 
 isType = require("isType");
 
@@ -19,16 +15,34 @@ formatValue = require("./formatValue");
 Formatting = require("./Formatting");
 
 type = Type("Formatter", function(value, options) {
-  var error;
-  this._tracer = Tracer("Formatter::call()");
-  try {
-    return this._format(value, options);
-  } catch (error1) {
-    error = error1;
-    return throwFailure(error, {
-      stack: this._tracer()
-    });
+  var label, parts, raw;
+  if (options == null) {
+    options = {};
   }
+  if (isType(options, String)) {
+    options = {
+      label: options
+    };
+  } else {
+    assertType(options, Object);
+  }
+  raw = steal(options, "raw", false);
+  label = steal(options, "label");
+  if (this._colors && !options.hasOwnProperty("colors")) {
+    options.colors = this._colors;
+  }
+  parts = Formatting(options);
+  if (label) {
+    parts.push(label);
+  }
+  if (!formatValue.call(parts, value)) {
+    formatObject.call(parts, value);
+  }
+  parts = parts.flush();
+  if (raw) {
+    return parts;
+  }
+  return parts.join("");
 });
 
 type.defineOptions({
@@ -37,42 +51,8 @@ type.defineOptions({
 
 type.defineValues(function(options) {
   return {
-    _colors: options.colors,
-    _tracer: null
+    _colors: options.colors
   };
-});
-
-type.defineMethods({
-  _format: function(value, options) {
-    var label, parts, raw;
-    if (options == null) {
-      options = {};
-    }
-    if (isType(options, String)) {
-      options = {
-        label: options
-      };
-    } else {
-      assertType(options, Object);
-    }
-    raw = steal(options, "raw", false);
-    label = steal(options, "label");
-    if (this._colors && !options.hasOwnProperty("colors")) {
-      options.colors = this._colors;
-    }
-    parts = Formatting(options);
-    if (label) {
-      parts.push(label);
-    }
-    if (!formatValue.call(parts, value)) {
-      formatObject.call(parts, value);
-    }
-    parts = parts.flush();
-    if (raw) {
-      return parts;
-    }
-    return parts.join("");
-  }
 });
 
 module.exports = type.build();
